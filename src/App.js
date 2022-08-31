7cf797247c823acebcd44782b3b38684b0df0fdb
AlexisNoukan <alexisnoukan@gmail.com>
2022-08-31 13:50:12 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './styles/App.css';
 import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Routes} from 'react-router-dom';
 import Home from './pages/Home.js';
-import Projet from './pages/Projet';
-import Skill from './pages/Skill';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
+import Projet from './pages/Projet.js';
+import Skill from './pages/Skill.js';
+import Contact from './pages/Contact.js';
+import NotFound from './pages/NotFound.js';
 
 function App() {
   return (
     <div className="App">
-<h1>tst</h1>
     <BrowserRouter>
-      <Route path="/" element={Home} />
-      <Route path="/projet" element={Projet} />
-      <Route path="/skill" element={Skill} />
-      <Route path="/contact" element={Contact} />
-      <Route element={NotFound} />
+      <Routes path="/" element={Home} />
+      <Routes path="/projet" element={Projet} />
+      <Routes path="/skill" element={Skill} />
+      <Routes path="/contact" element={Contact} />
+      <Routes element={NotFound} />
     </BrowserRouter>
 
     </div>
