08e36e2edb49f968bae0ff8eb3f553cbfabe18e6
AlexisNoukan <alexisnoukan@gmail.com>
2022-08-31 11:58:51 +0200

    valid

---
+++ src/components/Navigation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navigation.css'
+
+const Navigation = () => {
+    return (
+        <div className='top-bar-home'>
+            
+                <NavLink exact to ='/' className="navLink-home" >
+                    <div className='home-pic'>
+                        <img src="./media/chibi_20220823_162302.png" alt="profil-pic" className='pic'/>
+                    </div>
+                    <h2>Alexis Noukan</h2> 
+                </NavLink>
+               
+            <div className='top-bar'>
+                <ul>
+                    <li>
+                        <NavLink to='/' className='navLink' >
+                            <span>Home</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/projet' className='navLink' >
+                            <span>Projet</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/skill' className='navLink'>
+                            <span>Skill</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/contact' className='navLink'>
+                            <span>Contact</span>
+                        </NavLink>
+                    </li> 
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
