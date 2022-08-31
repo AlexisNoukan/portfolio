fa68c5b1999db34b55579a473f4a863585493e0e
AlexisNoukan <alexisnoukan@gmail.com>
2022-08-31 13:44:51 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './styles/App.css';
 import React from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home.js';
 import Projet from './pages/Projet';
 import Skill from './pages/Skill';
 import Contact from './pages/Contact';
@@ -10,13 +10,15 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <div className="App">
+<h1>tst</h1>
     <BrowserRouter>
-      <Route exact path="/" element={Home} />
-      <Route exact path="/projet" element={Projet} />
-      <Route exact path="/skill" element={Skill} />
-      <Route exact path="/contact" element={Contact} />
+      <Route path="/" element={Home} />
+      <Route path="/projet" element={Projet} />
+      <Route path="/skill" element={Skill} />
+      <Route path="/contact" element={Contact} />
       <Route element={NotFound} />
     </BrowserRouter>
+
     </div>
   );
 }
