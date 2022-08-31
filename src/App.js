37d28fb679e547625c434b7848b98ea5df7a49bf
AlexisNoukan <alexisnoukan@gmail.com>
2022-08-31 13:02:37 +0200

    changement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './styles/App.css';
 import React from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import Projet from './pages/Projet';
+import Skill from './pages/Skill';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
       <Route exact path="/" element={Home} />
+      <Route exact path="/projet" element={Projet} />
+      <Route exact path="/skill" element={Skill} />
+      <Route exact path="/contact" element={Contact} />
+      <Route element={NotFound} />
     </BrowserRouter>
     </div>
   );
