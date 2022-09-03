6cc1a4323ba77e89ade9d7986e6a9cfed13ec169
AlexisNoukan <alexisnoukan@gmail.com>
2022-09-03 13:47:36 +0200

    skill

--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import '../styles/App.css'
 
 const skill = () => {
     return (
         <>
             <Navigation/>
-            <p>ca marche</p>
+            
+            <p>HTML</p>
+            <div class="container">
+                <div class="skills html">90%</div>
+            </div>
+
+            <p>CSS</p>
+            <div class="container">
+                <div class="skills css">80%</div>
+            </div>
+
+            <p>JavaScript</p>
+            <div class="container">
+                <div class="skills js">65%</div>
+            </div>
+
+            <p>PHP</p>
+            <div class="container">
+                <div class="skills php">60%</div>
+            </div>
         </>
     );
 };
