3a5e7b6cbadf3ddf1f0e3b713eadc9d73375a4f2
AlexisNoukan <alexisnoukan@gmail.com>
2022-09-03 14:27:07 +0200

    css

--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -7,30 +7,30 @@ const skill = () => {
         <>
             <Navigation/>
             <div className='center'>
-            <p>HTML</p>
-            <div class="container">
-                <div class="skills html">90%</div>
-            </div>
+                <p>HTML</p>
+                <div class="container">
+                    <div class="skills html">90%</div>
+                </div>
 
-            <p>CSS</p>
-            <div class="container">
-                <div class="skills css">80%</div>
-            </div>
+                <p>CSS</p>
+                <div class="container">
+                    <div class="skills css">80%</div>
+                </div>
 
-            <p>JavaScript</p>
-            <div class="container">
-                <div class="skills js">65%</div>
-            </div>
+                <p>JavaScript</p>
+                <div class="container">
+                    <div class="skills js">65%</div>
+                </div>
 
-            <p>ReactJs</p>
-            <div class="container">
-                <div class="skills reactjs">60%</div>
-            </div>
+                <p>ReactJs</p>
+                <div class="container">
+                    <div class="skills reactjs">60%</div>
+                </div>
 
-            <p>Python</p>
-            <div class="container">
-                <div class="skills python">60%</div>
-            </div>
+                <p>Python</p>
+                <div class="container">
+                    <div class="skills python">60%</div>
+                </div>
             </div>
         </>
     );
