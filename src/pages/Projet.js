69d64db347aa5d61bef528d5063a829a70f41c19
AlexisNoukan <alexisnoukan@gmail.com>
2022-09-04 17:32:08 +0200

    css projet

--- src/pages/Projet.js
+++ src/pages/Projet.js
@@ -10,44 +10,44 @@ const projet = () => {
         <>
           <Navigation/>
           <div className="jcp">
-          <div className='center'>
-            <div className='card-container'>
-              <div className="card">
-                <img 
-                src={Python} 
-                alt="python"/>
-                <p>python project</p>
-              </div>
-              <div className="card">
-                <img 
-                src={JavaScript} 
-                alt="python"/>
-                <p>JavaScript project</p>
-              </div>
-              <div className="card">
-                <img 
-                src={ReactImg} 
-                alt="python"/>
-                <p>ReactJS project</p>
-              </div>
-              <div className="card">
-                <img 
-                src={Python} 
-                alt="python"/>
-                <p>python project</p>
-              </div>
-              <div className="card">
-                <img 
-                src={Python} 
-                alt="python"/>
-                <p>python project</p>
-              </div>
-              <div className="card">
-                <img 
-                src={Python} 
-                alt="python"/>
-                <p>python project</p>
-              </div>
+            <div className='center'>
+              <div className='card-container'>
+                <div className="card">
+                  <img 
+                  src={Python} 
+                  alt="python"/>
+                  <p>python project</p>
+                </div>
+                <div className="card">
+                  <img 
+                  src={JavaScript} 
+                  alt="python"/>
+                  <p>JavaScript project</p>
+                </div>
+                <div className="card">
+                  <img 
+                  src={ReactImg} 
+                  alt="python"/>
+                  <p>ReactJS project</p>
+                </div>
+                <div className="card">
+                  <img 
+                  src={Python} 
+                  alt="python"/>
+                  <p>python project</p>
+                </div>
+                <div className="card">
+                  <img 
+                  src={Python} 
+                  alt="python"/>
+                  <p>python project</p>
+                </div>
+                <div className="card">
+                  <img 
+                  src={Python} 
+                  alt="python"/>
+                  <p>python project</p>
+                </div>
             </div>
           </div>
         </div>
